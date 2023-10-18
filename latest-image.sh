3b706a66ded0fe0ff0b98bd327f175b845d7963a
rajeev <rajeev1026@gmail.com>
2023-10-18 17:31:39 +0000

    date change

--- latest-image.sh
+++ latest-image.sh
@@ -7,7 +7,7 @@ temp=0
 for tag in $taglist
 do 
     createdTime="$(docker inspect -f '{{ .Created }}' $1:$tag)"
-    formattedCT=$(date -d "$createdTime" +'%Y %m %d %H %M %S')
+    formattedCT=$(date -d "$createdTime" +'%Y%m%d%H%M%S')
     if [ $temp -eq 0 ]; then
         greatestValue=$formattedCT
         temp=1
