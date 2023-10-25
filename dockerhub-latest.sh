f11efdf9eb578f8979742cbeb5ab6947ae0c18c1
pavan1026 <52605330+pavan1026@users.noreply.github.com>
2023-10-25 12:25:46 +0100

    Update dockerhub-latest.sh

--- dockerhub-latest.sh
+++ dockerhub-latest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tag=$(curl -s "https://registry.hub.docker.com/v2/repositories/8179554905/env-app/tags" | jq --raw-output '.results[].name' | head -1)
+tag=$(curl -s "https://registry.hub.docker.com/v2/repositories/8179554905/development/tags" | jq --raw-output '.results[].name' | head -1)
  echo $tag
 
 latest="$(docker run 8179554905/env-app:$tag)"
